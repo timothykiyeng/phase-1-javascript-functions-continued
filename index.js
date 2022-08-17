b10acebe32fb382bea611a903e4568785817cf43
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-08-17 10:27:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog!");
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork("work from home.");
+
+let wrapAdjective = function (makePie = "*") {
+  return function (adjective = "special") {
+    return `You are ${makePie}${adjective}${makePie}!`;
+  };
+};
+
+wrapAdjective("%")("a dedicated programmer");
